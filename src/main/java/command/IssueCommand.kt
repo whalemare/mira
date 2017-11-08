e0e772f1d6bd027dc3b298229c67734e7cfbfadb
whalemare <nanostet@gmail.com>
2017-11-08 17:31:42 +0700

    Some refactor codestyle

--- src/main/java/command/IssueCommand.kt
+++ src/main/java/command/IssueCommand.kt
@@ -18,31 +18,29 @@ import repository.Repository
         footer = arrayOf("mira (c) 2017"))
 class IssueCommand(val repository: Repository) : Runnable {
 
-    @Option(
-            names = arrayOf("-i", "--id", "-id"),
-            description = arrayOf("Index of search issue. It`s terminate operation")
-    )
+    @Option(names = arrayOf("-i", "--id", "-id"),
+            description = arrayOf("Index of search issue. It`s terminate operation"))
     var id: Int = Int.MIN_VALUE
 
-    @Option(names = arrayOf("-p", "--print"), description = arrayOf("Print issue data. By default = true"))
+    @Option(names = arrayOf("-p", "--print"),
+            description = arrayOf("Print issue data. By default = true"))
     var print: Boolean = true
 
-    @Option(names = arrayOf("-a", "--all"), description = arrayOf("Show all available info"))
+    @Option(names = arrayOf("-a", "--all"),
+            description = arrayOf("Show all available info"))
     var showAll: Boolean = false
 
-    @Option(names = arrayOf("-m", "--me"), description = arrayOf("Filter issues by assigned to me"))
+    @Option(names = arrayOf("-m", "--me"),
+            description = arrayOf("Filter issues by assigned to me"))
     var assignedMe: Boolean = false
 
     @Option(names = arrayOf("-f", "--filter"),
             description = arrayOf("Filter issues with specific params",
-                    "See all available commands on http://www.redmine.org/projects/redmine/wiki/Rest_Issues")
-    )
+                    "See all available commands on http://www.redmine.org/projects/redmine/wiki/Rest_Issues"))
     var filter: MutableMap<String, String> = mutableMapOf()
 
-    @Option(
-            names = arrayOf("-s", "--start"),
-            description = arrayOf("Create and checkout branch, for issue defined by -i=##### operation")
-    )
+    @Option(names = arrayOf("-s", "--start"),
+            description = arrayOf("Create and checkout branch, for issue defined by -i=##### operation"))
     var start: Boolean = false
 
     override fun run() {
@@ -75,7 +73,6 @@ class IssueCommand(val repository: Repository) : Runnable {
         val command = "git checkout -b feature/#${issue.id}"
         BashExec(command).run()
     }
-
 
     fun print(issue: Issue?) {
         if (issue == null) {
